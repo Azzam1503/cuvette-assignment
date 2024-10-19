dd4c664ce8028a1631996066086ddd08a16f8b3a
AZZAM UDDIN <uazzam69@gmail.com>
2024-10-19 19:36:16 +0530

    updaed index.js

--- server/index.js
+++ server/index.js
@@ -12,10 +12,20 @@ dotenv.config();
 connectDB();
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({
-    origin: true,
-    credentials: true
-}));
+app.use(cors());
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "http://localhost://5173");
+    res.header(
+        "Access-Control-Allow-Methods",
+        "GET, HEAD, OPTIONS, POST, PUT, DELETE"
+    );
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    );
+    res.header("Access-Control-Allow-Credentials", "true");
+    next();
+})
 
 
 app.get("/", (req, res) => {
